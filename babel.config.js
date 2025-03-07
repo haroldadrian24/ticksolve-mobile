e051a04ec1cf245e9904051b60953a28f82a5577
petergok <peter@tempolabs.ai>
2025-03-07 14:42:13 -0500

    Updated template

--- babel.config.js
+++ babel.config.js
@@ -9,7 +9,8 @@ module.exports = function (api) {
 
   return {
     presets: [
-      "babel-preset-expo"
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
     ],
     plugins,
   };
