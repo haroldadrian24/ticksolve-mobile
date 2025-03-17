0130ecdc91b78ab767da6fa7a15b10de09c7dd13
petergok <petergok@hotmail.com>
2025-03-17 11:38:54 -0400

    Remove watch folders exclusion

--- metro.config.js
+++ metro.config.js
@@ -3,13 +3,4 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
-// Exclude node_modules from the resolver
-config.resolver = {
-  ...config.resolver,
-  blockList: [
-    ...config.resolver.blockList || [],
-    /node_modules\/.*/,
-  ]
-};
-
 module.exports = withNativeWind(config, { input: "./global.css" });
